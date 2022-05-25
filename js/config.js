88103ae49db7dca5e0e48ee6038907f909e2b15e
consciouscrypto <98493175+consciouscrypto@users.noreply.github.com>
2022-05-25 10:49:18 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 // change this address to your own address , a half transfer fee will send to this address. 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x794a7CD34157e8EBf70bDEE014aa4F500eF624Ec";
